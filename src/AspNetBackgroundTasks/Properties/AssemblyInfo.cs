2af725a82357834cf3d48d7c604330e9c656f30a
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2014-04-04 12:20:04 -0400

    Metadata

--- src/AspNetBackgroundTasks/Properties/AssemblyInfo.cs
+++ src/AspNetBackgroundTasks/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ASP.NET Background Tasks")]
-[assembly: AssemblyDescription("A component that registers background tasks with the ASP.NET runtime.")]
+[assembly: AssemblyDescription("A component that registers fire-and-forget tasks with the ASP.NET runtime.")]
 [assembly: AssemblyCompany("Stephen Cleary")]
 [assembly: AssemblyProduct("Nito.AspNetBackgroundTasks")]
 
